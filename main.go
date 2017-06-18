9ff1e025e20d33fd65ba1b4dadc63ac71a6d3c3f
Kevin Nelson <knelsonvt@gmail.com>
2017-06-18 00:17:10 -0400

    Created helper methods for viewing State matrixes

--- main.go
+++ main.go
@@ -1,8 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+//	"reflect"
+	"errors"
+)
+
+func PrintHexStateMatrix(m [][]byte) {
+	printStateMatrix(m, 0)
+}
+
+func PrintASCIIStateMatrix(m [][]byte) {
+	printStateMatrix(m, 1)
+}
+
+func printStateMatrix(m [][]byte, mode int) {
+	var format string
+
+	switch mode {
+	case 0:  // Hex
+		format = "%3X"
+	case 1:  // ASCII
+		format = "%3c"
+	default:
+		format = "%3X"
+	}
+
+	for _, row := range m {
+		for _, value := range row {
+			fmt.Printf(format, value)
+		}
+		fmt.Println()
+	}
+
+	fmt.Println()
+}
+
+func isMatrix() error {
+	err := errors.New("Value is not matrix")
+	return err
+}
+
+func createStateMatrix(size int) [][]byte {
+	stateMatrix := make([][]byte, size)
+
+	for i := range stateMatrix {
+		stateMatrix[i] = make([]byte, size)
+	}
+
+	return stateMatrix
+}
+
+func initializeStateMatrix(m [][]byte, secret string) {
+	for i, row := range m {
+		for j, _ := range row {
+			m[j][i] = secret[i * len(row) + j]
+		} 
+	}
+}
 
 func main() {
-	fmt.Printf("Test")
+	m := createStateMatrix(4)
+
+	initializeStateMatrix(m, "Secret, secret!!")
+
+	PrintHexStateMatrix(m)
+
+	PrintASCIIStateMatrix(m)
 }
 
